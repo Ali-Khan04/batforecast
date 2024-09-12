ec5a169c1e6875ea9459104c5e695524cc34b4b4
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2024-09-13 00:02:08 +0500

    Improved path handeling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import User from "./models/User.js";
 import path from "path";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
 
 // Load environment variables from confidential.env file
 dotenv.config({ path: "./confidential.env" });
@@ -12,17 +13,20 @@ const app = express();
 // Use environment variable for MongoDB URI
 mongoose.connect(process.env.MONGO_URI);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   const message = req.query.message || "";
-  res.sendFile(path.join(process.cwd(), "public", "login.html"));
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 app.get("/signup", (req, res) => {
   const message = req.query.message || "";
-  res.sendFile(path.join(process.cwd(), "public", "signup.html"));
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
 app.post("/signup", async function (req, res) {
@@ -51,14 +55,14 @@ app.post("/login", async function (req, res) {
       return res.redirect("/?message=Invalid username or password");
     }
 
-    res.sendFile(path.join(process.cwd(), "public", "weatherDesign.html"));
+    res.sendFile(path.join(__dirname, "public", "weatherDesign.html"));
   } catch (err) {
     res.send("Error during login");
   }
 });
 
 app.get("/weatherDesign", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public", "weatherDesign.html"));
+  res.sendFile(path.join(__dirname, "public", "weatherDesign.html"));
 });
 
 app.listen(3000, function () {
