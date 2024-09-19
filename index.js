b34a630defc729ce31fe6d65c908990c677e4df2
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2024-09-19 22:00:17 +0500

    Implemented password hashing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import User from "./models/User.js";
 import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-
+import bcrypt from "bcrypt";
 // Load environment variables from confidential.env file
 dotenv.config({ path: "./confidential.env" });
 
@@ -29,29 +29,35 @@ app.get("/signup", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
-app.post("/signup", async function (req, res) {
+const handleSignup = async (req, res) => {
   const { username, password } = req.body;
-
+  if (!username || !password) {
+    res.status(400).send("Please provide username and password");
+  }
   try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.redirect("/signup?message=Username already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
-      password,
+      password: hashedPassword,
     });
-
     await newUser.save();
     res.redirect("/?message=Signup successful! Please log in.");
   } catch (err) {
-    res.redirect("/signup?message=Error saving user. Please try again.");
+    res.status(500).send("Internal Server Error");
   }
-});
+};
 
 app.post("/login", async function (req, res) {
   const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ username });
-
-    if (!user || user.password !== password) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.redirect("/?message=Invalid username or password");
     }
 
@@ -60,6 +66,7 @@ app.post("/login", async function (req, res) {
     res.send("Error during login");
   }
 });
+app.post("/signup", handleSignup);
 
 app.get("/weatherDesign", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "weatherDesign.html"));
